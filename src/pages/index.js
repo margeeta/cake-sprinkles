4bbfb54a171b68691a9611db83530ae82a96863e
Ivy <iknight@phunware.com>
2020-06-24 10:22:34 -0500

    adding nav icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import data from "../../data.json"
-import iconItem from "../components/iconItem"
+
 const IndexPage = () => (
   <Layout>
     <hgroup id="hgroup">
